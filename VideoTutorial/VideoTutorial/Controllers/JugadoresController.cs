7752865a7ac5bb4e7e9829d1d13e52e27236ad88
Tomás Serrano Mata <TheMoonComeOut@gmail.com>
2019-02-09 19:31:20 +0100

    Version 1.0

---
+++ VideoTutorial/VideoTutorial/Controllers/JugadoresController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoTutorial.Models;
+
+namespace VideoTutorial.Controllers {
+    public class JugadoresController : Controller {
+        HelperJugadores helper;
+        public JugadoresController() {
+            this.helper = new HelperJugadores();
+        }
+        /*__________________________________________________*/
+        //Paginación con Linq, filtrando por equipos
+        // GET: PaginarJugadores
+        public ActionResult PaginarJugadores(int? posicion, String  equipos) {
+            if (posicion == null ) {
+                posicion = 0;
+            }
+            int totalregistros = 0;
+            List<InformacionJugadores> jugadores = helper.GetPaginarJugadores(equipos, posicion.GetValueOrDefault(), ref totalregistros);
+            ViewBag.TotalRegistros = totalregistros;
+            ViewBag.ListEquipo = helper.GetEquipos(); 
+            return View(jugadores);
+        }
+        //POST: PaginarJugadores
+        [HttpPost]
+        public ActionResult PaginarJugadores(String equipos, int? posicion) {
+            if (posicion == null || equipos != ViewBag.Equipos) {
+                posicion = 0;
+            }
+            int totalregistros = 0;
+            List<InformacionJugadores> jugadores = helper.GetPaginarJugadores(equipos, posicion.GetValueOrDefault(), ref totalregistros);
+            ViewBag.TotalRegistros = totalregistros;
+            ViewBag.ListEquipo = helper.GetEquipos();
+
+            ViewBag.Equipos = equipos;
+
+            return View(jugadores);
+        }
+        /*__________________________________________________*/
+        //Paginación con Linq sencilla
+        // GET: PaginarLinq
+        public ActionResult PaginarLinq(int? posicion){
+            if (posicion == null) {
+                posicion = 0;
+            }
+            int totalregistros = 0;
+            List<InformacionJugadores> jugadores = helper.GetPaginarLINQ(posicion.GetValueOrDefault(), ref totalregistros);
+            ViewBag.TotalRegistros = totalregistros;
+            return View(jugadores);
+        }
+    }
+}
---
+++ VideoTutorial/VideoTutorial/Models/HelperJugadores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoTutorial.Models {
+    public class HelperJugadores {
+        HOSPITALEntities entidad;
+        public HelperJugadores() {
+            this.entidad = new HOSPITALEntities();
+        }
+        /*-------------*/
+        public List<InformacionJugadores> GetPaginarJugadores(String equipos, int posicion, ref int totalregistros) {
+            if (equipos == null) {
+                totalregistros = (from datos in entidad.InformacionJugadores select datos).OrderBy(z => z.JNOMBRE).ToList().Count();
+                var consulta = (from datos in entidad.InformacionJugadores select datos).OrderBy(z => z.JNOMBRE).Skip(posicion).Take(3).ToList();
+                return consulta.ToList();
+            } else {
+                totalregistros = (from datos in entidad.InformacionJugadores where equipos.Contains((String)datos.NOM_EQUIPO) select datos).OrderBy(z => z.JNOMBRE).ToList().Count();
+                var consulta = (from datos in entidad.InformacionJugadores where equipos.Contains((String)datos.NOM_EQUIPO) select datos).OrderBy(z => z.JNOMBRE).Skip(posicion).Take(3).ToList();
+                return consulta.ToList();
+            }
+        }
+        
+        /*-------------*/
+        public List<EQUIPO> GetEquipos() {
+            var consulta = from datos in entidad.EQUIPO select datos;
+            List<EQUIPO> equipos = consulta.ToList();
+            return equipos;
+        }
+        /*-------------*/
+        public List<InformacionJugadores> GetPaginarLINQ(int posicion, ref int totalregistros) {
+            totalregistros = this.entidad.InformacionJugadores.Count();
+            var consulta = (from datos in entidad.InformacionJugadores select datos).OrderBy(z => z.JNOMBRE).Skip(posicion).Take(3).ToList();
+            List<InformacionJugadores> jugadores = consulta;
+            return jugadores;
+        }
+    }
+}
